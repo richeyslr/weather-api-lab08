647b36b2585b1adb1b409d98e9c4f7063d072ed7
Spencer Richey <richeyslr@gmail.com>
2021-10-12 17:47:40 -0500

    backend endpoints return correct data

---
+++ back/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const port = 3003;
+const cors = require("cors");
+
+app.use(cors());
+
+const weatherData = [];
+
+app.get("/weather", async (req, res) => {
+  try {
+    const lat = req.query.lat;
+    const lon = req.query.lon;
+    const url = `http://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=166433982f014efbad3f5c2171d8641a`
+    const result = await axios.get(url);
+    res.send(result.data)
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
